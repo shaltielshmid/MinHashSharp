fd0dea144e3f02debda2e5023980a6dd014baa40
Shaltiel Shmidman <shaltieltzion@gmail.com>
2023-10-28 23:22:56 +0300

    Initial commit

---
+++ MinHashSharp/MinHashLSH.cs
@@ -0,0 +1,141 @@
+﻿using MathNet.Numerics.Integration;
+using System.Runtime.InteropServices;
+
+namespace MinHashSharp {
+    public class MinHashLSH {
+        private readonly int _numPerm;
+        private readonly int _numBuckets;
+        private readonly int _bucketRange;
+        private readonly List<Dictionary<string, HashSet<string>>> _hashTables;
+        private readonly List<(int start, int end)> _hashRanges;
+        private readonly HashSet<string> _keys;
+
+        /// <summary>
+        /// The `MinHash_LSH` index. 
+        /// Supporting for big-data fast querying using an approximate `Jaccard similarity` threshold 
+        /// </summary>
+        /// <param name="threshold">The Jaccard similarity threshold between 0.0 and 1.0. The initialized MinHash LSH will optimize the bucket count and ranges to minimize the false positives and false negatives.</param>
+        /// <param name="numPerm">The number of permutation functions used by the MinHash to be indexed.</param>
+        public MinHashLSH(double threshold, int numPerm) : this(threshold, numPerm, (0.5, 0.5)) {
+        }
+
+        /// <summary>
+        /// The `MinHash_LSH` index. 
+        /// Supporting for big-data fast querying using an approximate `Jaccard similarity` threshold 
+        /// </summary>
+        /// <param name="threshold">The Jaccard similarity threshold between 0.0 and 1.0. The initialized MinHash LSH will optimize the bucket count and ranges to minimize the false positives and false negatives.</param>
+        /// <param name="numPerm">The number of permutation functions used by the MinHash to be indexed.</param>
+        /// <param name="weights">The weights to apply to the bucket optimization for fp (false-positive) and fn (false-negative) matches. Defaults to (0.5, 0.5)</param>
+        public MinHashLSH(double threshold, int numPerm, (double fp, double fn) weights) : this(numPerm, CalculateOptimalBucketParams(threshold, numPerm, weights)) {
+        }
+
+        private MinHashLSH(int numPerm, (int numBuckets, int bucketRange) param) {
+            if (numPerm < 2)
+                throw new ArgumentOutOfRangeException(nameof(numPerm), "Must be >=2");
+            if (param.numBuckets * param.bucketRange > numPerm)
+                throw new ArgumentOutOfRangeException(nameof(param), "Product of bucket * range must be less than numPerm");
+            if (param.numBuckets < 2)
+                throw new ArgumentOutOfRangeException(nameof(param.numBuckets), "Must be >=2");
+
+            _numPerm = numPerm;
+            _numBuckets = param.numBuckets;
+            _bucketRange = param.bucketRange;
+            _keys = new();
+            _hashTables = Enumerable.Range(0, _numBuckets).Select(_ => new Dictionary<string, HashSet<string>>()).ToList();
+            _hashRanges = Enumerable.Range(0, _numBuckets).Select(i => (i * _bucketRange, (i + 1) * _bucketRange)).ToList();
+        }
+        /// <summary>
+        /// Insert a key to the index, together with a MinHash of the set referenced by a unique key.
+        /// A list of keys will be the return value when querying for matches. 
+        /// </summary>
+        /// <param name="key">A unique key representing the hash</param>
+        /// <param name="mh">The MinHash object</param>
+        /// <exception cref="ArgumentException"></exception>
+        public void Insert(string key, MinHash mh) {
+            if (mh.Length != _numPerm)
+                throw new ArgumentException("Permutation length of minhash doesn't match expected.", nameof(mh));
+            if (_keys.Contains(key))
+                throw new ArgumentException("Key already exists", nameof(key));
+
+            _keys.Add(key);
+            // Calculate the representation of each hash range and index them
+            for (int i = 0; i < _numBuckets; i++) {
+                var h = CreateRepresentationOfHashValues(mh.HashValues(_hashRanges[i].start, _hashRanges[i].end));
+
+                if (!_hashTables[i].ContainsKey(h))
+                    _hashTables[i].Add(h, new());
+                _hashTables[i][h].Add(key);
+            }// next bucket
+        }
+
+        /// <summary>
+        /// Giving the MinHash of the query set, retrieve the keys that reference sets with
+        /// Jaccard similarities likely greater than the threshold.
+        /// Results are based on minhash segment collision and are thus approximate.
+        /// For more accurate results, filter again with: `MinHash.jaccard`.
+        /// </summary>
+        /// <param name="mh">The MinHash object to find approximate matches for</param>
+        /// <returns>An enumerable of `keys` that reference MinHash objects that have a Jaccard similarity >= threshold (approximate)</returns>
+        /// <exception cref="ArgumentException"></exception>
+        public IEnumerable<string> Query(MinHash mh) {
+            if (mh.Length != _numPerm)
+                throw new ArgumentException("Permutation length of minhash doesn't match expected.", nameof(mh));
+
+            // Calculate the representation of each hash range and check for matches in our table
+            for (int i = 0; i < _numBuckets; i++) {
+                var h = CreateRepresentationOfHashValues(mh.HashValues(_hashRanges[i].start, _hashRanges[i].end));
+
+                if (_hashTables[i].ContainsKey(h)) {
+                    foreach (string key in _hashTables[i][h])
+                        yield return key;
+                }
+            }// next bucket
+        }
+
+        public int Count => _keys.Count;
+
+        private static string CreateRepresentationOfHashValues(uint[] vals) {
+            // Convert the uints to chars, and then convert to string
+            // This isn't perfect, but we tested 6 different ways to index the hash values,
+            // and this was the fastest by a large factor. 
+            return new string(MemoryMarshal.Cast<uint, char>(vals));
+        }
+
+        private static (int b, int r) CalculateOptimalBucketParams(double threshold, int numPerm, (double fp, double fn) weights) {
+            // Validations:
+            if (threshold is > 1 or < 0)
+                throw new ArgumentOutOfRangeException(nameof(threshold), "Must be in [0.0, 1.0]");
+            if (weights.fn is < 0 or > 1 || weights.fp is < 0 or > 1)
+                throw new ArgumentOutOfRangeException(nameof(weights), "Must be in [0.0, 1.0]");
+            if (Math.Round(weights.fn + weights.fp, 1) != 1.0)
+                throw new ArgumentOutOfRangeException(nameof(weights), "Must sum to 1.0");
+
+            double minError = double.MaxValue;
+            var opt = (0, 0);// optimal value to return, initialize to zeros
+
+            // Figure out the optimal way to choose the number buckets and the bucket range
+            // Go through and calculate the error for each permutation
+            for (int b = 1; b <= numPerm; b++) {
+                int maxR = numPerm / b;
+                for (int r = 1; r <= maxR; r++) {
+                    // Calculate the false positive and negative probabilities by integrating over the
+                    // error function for each, and choose the set that produces the smallest error.
+                    // fp = f(x) = 1 - ((1 - x^r)^b), integrated from 0 to thresh
+                    var fpProb = GaussLegendreRule.Integrate(x => 1 - Math.Pow(1 - Math.Pow(x, r), b), 0, threshold, 5);
+                    // fn = f(x) = 1 - fp(x), integrated from thresh to 1
+                    var fnProb = GaussLegendreRule.Integrate(x => Math.Pow(1 - Math.Pow(x, r), b), threshold, 1, 5);
+
+                    // Combine the probabilities using the weights that were given (e.g., one can prioritize
+                    // having fewer false positives at the cost of false negatives)
+                    var error = fpProb * weights.fp + fnProb * weights.fn;
+                    if (error < minError) {
+                        opt = (b, r);
+                        minError = error;
+                    }
+                }// next range size
+            }// next bucket size
+
+            return opt;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# MinHashSharp
+# MinHashSharp - A simple lightweight library for similarity estimation
+
+`MinHashSharp` offers a simple lightweight data structure for indexing and estimating Jaccard similarity between sets. 
+
+Was tested with 60GB of data and tens of millions of documents, and ran smoothly and efficiently. 
+
+The library currently offers two classes:
+
+`MinHash`: A probabilistic data structure for computing Jaccard similarity between sets. 
+
+`MinHashLSH` A class for supporting big-data fast querying using an approximate `Jaccard similarity` threshold.
+
+## Sample usage
+
+```cs
+string s1 = "The quick brown fox jumps over the lazy dog and proceeded to run towards the other room";
+string s2 = "The slow purple elephant runs towards the happy fox and proceeded to run towards the other room";
+string s3 = "The quick brown fox jumps over the angry dog and proceeded to run towards the other room";
+
+var m1 = new MinHash(numPerm: 128).Update(s1.Split());
+var m2 = new MinHash(numPerm: 128).Update(s2.Split());
+var m3 = new MinHash(numPerm: 128).Update(s3.Split());
+
+Console.WriteLine(m1.Jaccard(m2));// 0.51
+
+var lsh = new MinHashLSH(threshold: 0.8, numPerm: 128);
+
+lsh.Insert("s1", m1);
+lsh.Insert("s2", m2);
+
+Console.WriteLine(string.Join(", ", lsh.Query(m3))); // s1
+```
+
+## Multi-threading
+
+The library is entirely thread-safe except for the `MinHashLSH.Insert` function (and the custom injected hash function, if relevant). Therefore, you can create `MinHash` objects on multiple threads and query the same `MinHashLSH` object freely. If you are indexing sets on multiple threads, then just make sure to gain exclusive access to the LSH around every `Insert` call:
+
+```cs
+lock (lsh) {
+    lsh.Insert("s3", m3);
+}
+```
+
+## Custom hash function
+
+By default, the library uses the [Farmhash function](https://opensource.googleblog.com/2014/03/introducing-farmhash.html) introduced by Google for effeciency. For more accurate hashes, one can inject a custom hash function into the `MinHash` object.
+
+For example, if you want to use the C# default string hash function:
+
+```cs
+static uint StringHash(string s) => (uint)s.GetHashCode();
+
+var m = new MinHash(numPerm: 128, hashFunc: StringHash).Update(s1.Split());
+```
